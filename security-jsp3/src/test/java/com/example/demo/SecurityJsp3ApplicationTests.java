c3f4f39d250c9b472da82e51f09402405aa069e8
AnnieKMoon <mkw005@naver.com>
2026-01-12 11:45:29 +0900

    인가실습

--- security-jsp3/src/test/java/com/example/demo/SecurityJsp3ApplicationTests.java
+++ security-jsp3/src/test/java/com/example/demo/SecurityJsp3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SecurityJspApplicationTests {
+class SecurityJsp3ApplicationTests {
 
 	@Test
 	void contextLoads() {
